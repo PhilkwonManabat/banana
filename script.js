e51baa811458059a0d9e6e2894607534e93351aa
PhilkwonManabat <131845141+PhilkwonManabat@users.noreply.github.com>
2024-02-24 13:22:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn')
+const noBtn = document.querySelector('.no-btn')
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'I Love Bananas Too ツ';
+});
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
